d2d5c418ebd26391330b1476c1a8ddee951d1c93
apocalypse575 <apocalypse575@gmail.com>
2017-12-04 01:15:13 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,6 +6,9 @@ def assign_variable(value)
 
 end
 
+#rspec ./spec/ruby_basics_spec.rb:17 # ruby
+#assign_variable takes an argument of a persons name and assign it to a variable name
+
 def argue
 
 end
